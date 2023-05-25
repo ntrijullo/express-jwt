cde78a19377910678afd577e678cfbe48db38d82
ntrijullo <enzot.13@gmail.com>
2023-05-25 00:17:37 -0400

    add controller and routes

---
+++ src/controllers/users.js
@@ -0,0 +1,21 @@
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt')
+const user = require('../models/users')
+
+const getAdmin = (req, res) =>{
+
+}
+
+const login = (req, res) =>{
+
+}
+
+const register = (req, res) =>{
+
+}
+
+module.exports = {
+    getAdmin,
+    login,
+    register
+}
--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const connection = require('./connect')
+const userRoutes = require('./routes/users')
 const app = express()
 
 app.set('port',3000)
 app.use(express.urlencoded({extended: false}))
+app.use(userRoutes)
 app.listen(app.get('port'),()=>{
     console.log('App sobre puerto '+app.get('port'))
 })
